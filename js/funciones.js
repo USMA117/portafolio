99cfa35c40da33ec2913624968ce4c2aa5a04688
Sebastian Usma <johan58usma@gmail.com>
2024-06-29 16:57:00 -0500

    Eliminando impresiones de prueba

--- js/funciones.js
+++ js/funciones.js
@@ -5,21 +5,18 @@ let texto = "Sebastian Usma";
 function crear_efecto_typing(elemento,texto_p,i=0){
         elemento.textContent += texto_p[i];
         if(i == texto.length ){
-            console.warn("fin del typing")
             i = -1;
             elemento.textContent = "";
         }
             setTimeout(()=>{
                 crear_efecto_typing(texto_efecto_typing,texto,i+1);
             },200);
-            console.log(texto_p[i]);
-        
         
 }
 crear_efecto_typing(texto_efecto_typing,texto);
 
 
 textarea.addEventListener('click', () => {
-    textarea.setSelectionRange(0, 0); // Coloca el cursor al inicio
-    textarea.focus(); // Asegura que el textarea tiene el foco
+    textarea.setSelectionRange(0, 0);
+    textarea.focus();
 });
